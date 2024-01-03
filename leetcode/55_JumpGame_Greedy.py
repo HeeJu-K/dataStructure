77adc8145b7e31c3bc1ccc482622a7bb6dd9630a
HeeJu Kim <kmju99@naver.com>
2024-01-03 02:37:29 -0800

    leetcode 55, 1d array, greedy

--- leetcode/55_JumpGame_Greedy.py
+++ leetcode/55_JumpGame_Greedy.py
@@ -1,12 +1,21 @@
 ### Greedy Algorithm
 
 ### move goal to the front one by one if the i-1 can be reached
+# class Solution:
+#     def canJump(self, nums: List[int]) -> bool:
+#         goal = len(nums)-1
+
+#         for i in range (len(nums)-2, -1, -1):
+#             if i+nums[i]>= goal:
+#                 goal = i
+
+#         return True if goal==0 else False
+
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        goal = len(nums)-1
-
-        for i in range (len(nums)-2, -1, -1):
-            if i+nums[i]>= goal:
-                goal = i
-
-        return True if goal==0 else False
+        l = len(nums)
+        pos = l-1
+        for i in range (l-1, -1, -1):
+            if i+nums[i] >= pos:
+                pos = i
+        return pos == 0
