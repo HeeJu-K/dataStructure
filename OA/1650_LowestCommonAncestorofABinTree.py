f2dd4614fdf0ccdf58fc22495ad37ee7c56f6c2f
HeeJu Kim <kmju99@naver.com>
2024-01-08 00:06:50 -0800

    leetcode 1650, binary tree traversal

---
+++ OA/1650_LowestCommonAncestorofABinTree.py
@@ -0,0 +1,18 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.parent = None
+"""
+class Solution:
+    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+        seen = set()
+        while p:
+            seen.add(p.val)
+            p = p.parent
+        while q and q.val not in seen:
+            q = q.parent
+        return q
