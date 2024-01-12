be6d296615bf7466411ff0be7131ed76ef521fca
HeeJu Kim <kmju99@naver.com>
2024-01-11 18:08:53 -0800

    339, list, dfs

---
+++ leetcode/339_NestedListWeightSum.py
@@ -0,0 +1,60 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        nums = []
+        weights = []
+        def dfs(item, depth):
+            if item.isInteger():
+                nums.append(item.getInteger())
+                weights.append(depth)
+            else:
+                for nestedItem in item.getList():
+                    dfs(nestedItem, depth+1)
+        for item in nestedList:
+            dfs(item, 1)
+        res = 0
+        for i in range(len(nums)):
+            res += nums[i]*weights[i]
+        return res
