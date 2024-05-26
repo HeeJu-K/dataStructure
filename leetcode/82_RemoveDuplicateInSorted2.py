fff55f2dbebeb4e86fd0cbd37db19e4eaa2ffa13
HeeJu Kim <kmju99@naver.com>
2024-05-26 11:33:34 -0700

    leetcode 82, list
    looked at solution. point is to use prev.next until safe, not updating the prev itself

--- leetcode/82_RemoveDuplicateInSorted2.py
+++ leetcode/82_RemoveDuplicateInSorted2.py
@@ -5,23 +5,46 @@
 #         self.next = next
 
 ### make another listnode that next is head, to avoid cases which first element of the head is already a duplicate so it has to be eliminated. In such an edge case head have to be dealt with bu itself, as we are returning the head node of the duplicated-removed listnode
+# class Solution:
+#     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         result = ListNode()
+#         result.next = head
+#         cur = result
+
+#         while head and head.next:
+#             # print("CUR--", cur)
+#             # print("HEAD--", head)
+#             # print("RESULT--", result)
+#             if head.val == head.next.val:
+#                 while head and head.next and head.val == head.next.val:
+#                     head = head.next
+#                 head = head.next
+#                 cur.next = head
+#             else:
+#                 cur = cur.next
+#                 head = head.next
+            
+#         return result.next
+    
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# point is to use prev.next until safe, not updating the prev itself
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        result = ListNode()
-        result.next = head
-        cur = result
-
-        while head and head.next:
-            # print("CUR--", cur)
-            # print("HEAD--", head)
-            # print("RESULT--", result)
-            if head.val == head.next.val:
-                while head and head.next and head.val == head.next.val:
-                    head = head.next
-                head = head.next
-                cur.next = head
-            else:
-                cur = cur.next
-                head = head.next
+        dummy = ListNode(0, head)
+        prev = dummy
+        while head:
             
-        return result.next
+            if head.next and head.val == head.next.val: # move prev when not equal
+                while head.next and head.val == head.next.val:
+                    head = head.next
+                prev.next = head.next
+            else:
+                prev = prev.next
+            head = head.next
+
+        return dummy.next
